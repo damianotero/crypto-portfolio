44bbe2fada434650f511547e96ec05d3164167d5
Damian <oteromorales@gmail.com>
2020-02-11 23:23:05 +0100

    refactor and adding more test

--- src/main/java/com/damian/cryptoportfolio/logic/services/UserService.java
+++ src/main/java/com/damian/cryptoportfolio/logic/services/UserService.java
@@ -1,8 +1,11 @@
 package com.damian.cryptoportfolio.logic.services;
 
 import com.damian.cryptoportfolio.data.UserRepository;
+import com.damian.cryptoportfolio.logic.exceptions.UserAlreadyExistsException;
 import com.damian.cryptoportfolio.logic.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,22 +16,27 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
-    public void addUser(User user) {
-        userRepository.addUser(user);
+    public User addUser(User user) {
+        try {
+            return userRepository.addUser(user);
+        }catch (DataIntegrityViolationException e){
+            throw new UserAlreadyExistsException("User Already Exists!!!", e);
+        }
     }
 
     public List<User> getUsers() {
         return userRepository.getUsers();
     }
 
-    public boolean validateUser(User requestedUser) {
-        return userRepository.validateUser(requestedUser);
-
+    public boolean userExists(User requestedUser) {
+        return userRepository.userExists(requestedUser);
     }
 
-
-    public User getUserByName (String name){
-        return userRepository.getUserByName(name);
+    public User findUserByName(String name){
+        try {
+           return userRepository.findUserByName(name);
+        } catch (EmptyResultDataAccessException e){
+            return null;
+        }
     }
 }
