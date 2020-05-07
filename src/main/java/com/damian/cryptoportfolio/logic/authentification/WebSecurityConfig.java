a3ea178424623a0afcc07daf777046101bce4623
damianotero <45541633+damianotero@users.noreply.github.com>
2020-05-07 20:07:24 +0200

    change css styles and nav bar (#6)

--- src/main/java/com/damian/cryptoportfolio/logic/authentification/WebSecurityConfig.java
+++ src/main/java/com/damian/cryptoportfolio/logic/authentification/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login*").permitAll()
                 .antMatchers("/*").permitAll()
                 .antMatchers("/css/**").permitAll()
+                .antMatchers("/images/**").permitAll()
                 .antMatchers("/users/addUser*").permitAll()
                 .anyRequest().authenticated()
                 .and()
